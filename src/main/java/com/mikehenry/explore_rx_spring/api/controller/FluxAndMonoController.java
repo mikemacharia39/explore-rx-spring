b2ae9b636cd5a61fb8a1329b6cf1f009772ce248
mikemacharia39 <mikemacharia39@gmail.com>
2025-01-13 17:02:43 +0300

    Add new flux method

--- src/main/java/com/mikehenry/explore_rx_spring/api/controller/FluxAndMonoController.java
+++ src/main/java/com/mikehenry/explore_rx_spring/api/controller/FluxAndMonoController.java
@@ -2,9 +2,14 @@ package com.mikehenry.explore_rx_spring.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class FluxAndMonoController {
 
     @GetMapping("/flux")
+    public Flux<Integer> returnFlux() {
+        return Flux.just(1, 2, 3, 4)
+                .log();
+    }
 }
