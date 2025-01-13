b2d44ab4e42c0fd7723729330ad9457b51e90c31
mikemacharia39 <mikemacharia39@gmail.com>
2025-01-13 18:07:43 +0300

    integration tests for endpoint that can return infinite response

--- src/test/java/com/mikehenry/explore_rx_spring/api/controller/FluxAndMonoControllerTest.java
+++ src/test/java/com/mikehenry/explore_rx_spring/api/controller/FluxAndMonoControllerTest.java
@@ -1,13 +1,16 @@
 package com.mikehenry.explore_rx_spring.api.controller;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+
+import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class FluxAndMonoControllerTest {
@@ -19,7 +22,7 @@ class FluxAndMonoControllerTest {
     void fluxTest_approach1() {
         Flux<Integer> integerFlux = webTestClient.get().uri("/flux")
                 .accept(MediaType.APPLICATION_JSON)
-                .exchange()
+                .exchange()// Allows us to make the call to the endpoint
                 .expectStatus().isOk()
                 .returnResult(Integer.class)
                 .getResponseBody();
@@ -30,5 +33,57 @@ class FluxAndMonoControllerTest {
                 .expectNext(3)
                 .expectNext(4)
                 .verifyComplete();
+    }
+
+    @Test
+    void fluxTest_approach2() {
+        webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_VALUE)
+                .expectBodyList(Integer.class)
+                .hasSize(4);
+    }
+
+    @Test
+    void fluxTest_approach3() {
+        EntityExchangeResult<List<Integer>> entityExchangeResult = webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Integer.class)
+                .returnResult();
+        List<Integer> expectedIntegerList = List.of(1, 2, 3, 4);
+        Assertions.assertEquals(expectedIntegerList, entityExchangeResult.getResponseBody());
+    }
+
+    @Test
+    void fluxTest_approach4() {
+        webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Integer.class)
+                .consumeWith(response -> {
+                    Assertions.assertEquals(List.of(1, 2, 3, 4), response.getResponseBody());
+                });
+    }
+
+    @Test
+    void fluxInfiniteTest_approach1() {
+        Flux<Long> longFlux = webTestClient.get().uri("/flux-infinite-stream")
+                .accept(MediaType.APPLICATION_NDJSON)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Long.class)
+                .getResponseBody();
+
+        StepVerifier.create(longFlux)
+                .expectNext(0L)
+                .expectNext(1L)
+                .expectNext(2L)
+                .thenCancel()
+                .verify();
     }
 }
