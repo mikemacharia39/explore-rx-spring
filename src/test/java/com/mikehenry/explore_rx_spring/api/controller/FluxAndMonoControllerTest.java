c28d16c3d16101d6e623b05115518c84979eda78
mikemacharia39 <mikemacharia39@gmail.com>
2025-01-13 17:40:39 +0300

    Writing tests for endpoints

--- src/test/java/com/mikehenry/explore_rx_spring/api/controller/FluxAndMonoControllerTest.java
+++ src/test/java/com/mikehenry/explore_rx_spring/api/controller/FluxAndMonoControllerTest.java
@@ -1,4 +1,34 @@
 package com.mikehenry.explore_rx_spring.api.controller;
 
-public class FluxAndMonoControllerTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class FluxAndMonoControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void fluxTest_approach1() {
+        Flux<Integer> integerFlux = webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(integerFlux)
+                .expectNext(1)
+                .expectNext(2)
+                .expectNext(3)
+                .expectNext(4)
+                .verifyComplete();
+    }
 }
