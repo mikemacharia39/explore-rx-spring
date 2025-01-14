e20b42436c81b7bb9b57ad45903407857931cd07
mikemacharia39 <mikemacharia39@gmail.com>
2025-01-14 19:56:30 +0300

    Reactive spring boot improvements

--- src/main/java/com/mikehenry/explore_rx_spring/api/dto/StudentSearchParams.java
+++ src/main/java/com/mikehenry/explore_rx_spring/api/dto/StudentSearchParams.java
@@ -4,8 +4,11 @@ import com.mikehenry.explore_rx_spring.domain.enumeration.Gender;
 import com.mikehenry.explore_rx_spring.domain.enumeration.LearningScheduleType;
 import lombok.Builder;
 
+import java.util.List;
+
 @Builder
 public record StudentSearchParams(
+        List<String> ids,
         String firstName,
         String lastName,
         String email,
