fd17de612dee5260437c5d68464ad1a103277885
mikemacharia39 <mikemacharia39@gmail.com>
2025-01-14 19:56:29 +0300

    Reactive spring boot improvements

--- src/main/java/com/mikehenry/explore_rx_spring/api/dto/AddressDto.java
+++ src/main/java/com/mikehenry/explore_rx_spring/api/dto/AddressDto.java
@@ -1,19 +1,24 @@
 package com.mikehenry.explore_rx_spring.api.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
-@Builder
-public record AddressDto(
-        @NotBlank
-        String country,
-        @NotBlank
-        String city,
-        @NotBlank
-        String addressLine1,
-        @NotBlank
-        String postalBox,
-        @NotBlank
-        String postalCode
-) {
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+public class AddressDto {
+    @NotBlank
+    String country;
+    @NotBlank
+    String city;
+    @NotBlank
+    String addressLine1;
+    @NotBlank
+    String postalBox;
+    @NotBlank
+    String postalCode;
 }
