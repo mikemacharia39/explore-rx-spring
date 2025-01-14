002aaa253ca964ec93f16f4ffbd7f1b48b37bb8f
mikemacharia39 <mikemacharia39@gmail.com>
2025-01-14 17:04:04 +0300

    add a service and controller

--- src/main/java/com/mikehenry/explore_rx_spring/domain/repository/StudentRepository.java
+++ src/main/java/com/mikehenry/explore_rx_spring/domain/repository/StudentRepository.java
@@ -2,8 +2,10 @@ package com.mikehenry.explore_rx_spring.domain.repository;
 
 import com.mikehenry.explore_rx_spring.domain.entity.Student;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
+@Repository
 public interface StudentRepository extends ReactiveMongoRepository<Student, String>, StudentRepositoryCustom {
 
     Flux<Student> findByFirstNameStartsWithOrLastNameStartsWithOrEmailStartsWith(String firstName,
