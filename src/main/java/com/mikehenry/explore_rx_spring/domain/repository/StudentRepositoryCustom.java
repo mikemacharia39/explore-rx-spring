f89318b1f9dbafcd21801c9880be4098a79f3f1f
mikemacharia39 <mikemacharia39@gmail.com>
2025-01-14 17:04:05 +0300

    add a service and controller

--- src/main/java/com/mikehenry/explore_rx_spring/domain/repository/StudentRepositoryCustom.java
+++ src/main/java/com/mikehenry/explore_rx_spring/domain/repository/StudentRepositoryCustom.java
@@ -1,19 +1,13 @@
 package com.mikehenry.explore_rx_spring.domain.repository;
 
+import com.mikehenry.explore_rx_spring.api.dto.StudentSearchParams;
 import com.mikehenry.explore_rx_spring.domain.entity.Student;
-import com.mikehenry.explore_rx_spring.domain.enumeration.Gender;
-import com.mikehenry.explore_rx_spring.domain.enumeration.LearningScheduleType;
 import org.springframework.data.domain.Pageable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface StudentRepositoryCustom {
-    Flux<Student> searchStudent(String firstName,
-                                String lastName,
-                                String email,
-                                Gender gender,
-                                LearningScheduleType scheduleType,
-                                String search,
+    Flux<Student> searchStudent(StudentSearchParams searchParams,
                                 Pageable pageable);
 
 
