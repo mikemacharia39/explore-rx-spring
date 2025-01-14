37d09148c9e45f02481eedef84ab10a45686dbf0
mikemacharia39 <mikemacharia39@gmail.com>
2025-01-14 19:56:30 +0300

    Reactive spring boot improvements

--- src/main/java/com/mikehenry/explore_rx_spring/api/dto/StudentResponseDto.java
+++ src/main/java/com/mikehenry/explore_rx_spring/api/dto/StudentResponseDto.java
@@ -2,20 +2,31 @@ package com.mikehenry.explore_rx_spring.api.dto;
 
 import com.mikehenry.explore_rx_spring.domain.enumeration.Gender;
 import com.mikehenry.explore_rx_spring.domain.enumeration.LearningScheduleType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.util.List;
 
-public record StudentResponseDto(
-        Long id,
-        String firstName,
-        String lastName,
-        Gender gender,
-        LocalDate dateOfBirth,
-        String email,
-        LearningScheduleType scheduleType,
-        AddressDto address,
-        List<String> subjects,
-        BigDecimal totalSpentInBooks
-){}
+@NoArgsConstructor
+@Setter
+@Getter
+@SuperBuilder
+public class StudentResponseDto {
+    String id;
+    String firstName;
+    String lastName;
+    Gender gender;
+    LocalDate dateOfBirth;
+    String email;
+    LearningScheduleType scheduleType;
+    AddressDto address;
+    List<String> subjects;
+    BigDecimal totalSpentInBooks;
+    Instant dateCreated;
+}
+
