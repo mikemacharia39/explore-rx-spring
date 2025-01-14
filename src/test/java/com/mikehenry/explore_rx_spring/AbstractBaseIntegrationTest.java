17007c055c9594ba3c986e5a85552792bf167a69
mikemacharia39 <mikemacharia39@gmail.com>
2025-01-14 20:29:45 +0300

    Abstract class for container setup

---
+++ src/test/java/com/mikehenry/explore_rx_spring/AbstractBaseIntegrationTest.java
@@ -0,0 +1,23 @@
+package com.mikehenry.explore_rx_spring;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class AbstractBaseIntegrationTest {
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:5.0.31-rc0-nanoserver")
+            .withExposedPorts(27017);
+
+    @DynamicPropertySource
+    static void containerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.host", mongoDBContainer::getHost);
+        registry.add("spring.data.mongodb.port", mongoDBContainer::getFirstMappedPort);
+    }
+}
