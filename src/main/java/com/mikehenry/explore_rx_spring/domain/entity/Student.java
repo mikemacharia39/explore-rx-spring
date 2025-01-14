d1dead9b8beefb37ad79a6293f5268b6d6ec9c30
mikemacharia39 <mikemacharia39@gmail.com>
2025-01-14 19:56:31 +0300

    Reactive spring boot improvements

--- src/main/java/com/mikehenry/explore_rx_spring/domain/entity/Student.java
+++ src/main/java/com/mikehenry/explore_rx_spring/domain/entity/Student.java
@@ -3,10 +3,11 @@ package com.mikehenry.explore_rx_spring.domain.entity;
 
 import com.mikehenry.explore_rx_spring.domain.enumeration.Gender;
 import com.mikehenry.explore_rx_spring.domain.enumeration.LearningScheduleType;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -16,24 +17,25 @@ import java.time.Instant;
 import java.time.LocalDate;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@SuperBuilder
 @Document(collection = "students")
 public class Student {
-
     @Id
     private String id;
     private String firstName;
     private String lastName;
     private Gender gender;
     private LocalDate dateOfBirth;
-    @Indexed(unique = true)
+    @Indexed(unique = true, name = "student-email-index")
     private String email;
+    @Builder.Default
     private LearningScheduleType scheduleType = LearningScheduleType.FULL_TIME;
     private Address address;
     private List<String> subjects;
     private BigDecimal totalSpentInBooks;
-    private Instant dateCreated;
+    @Builder.Default
+    private Instant dateCreated = Instant.now();
 }
