bbd2c7f89c2ddfb6102060ee944b28126cd6593c
mikemacharia39 <mikemacharia39@gmail.com>
2025-01-14 19:56:31 +0300

    Reactive spring boot improvements

--- src/main/java/com/mikehenry/explore_rx_spring/domain/repository/StudentRepositoryCustom.java
+++ src/main/java/com/mikehenry/explore_rx_spring/domain/repository/StudentRepositoryCustom.java
@@ -2,13 +2,11 @@ package com.mikehenry.explore_rx_spring.domain.repository;
 
 import com.mikehenry.explore_rx_spring.api.dto.StudentSearchParams;
 import com.mikehenry.explore_rx_spring.domain.entity.Student;
-import org.springframework.data.domain.Pageable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface StudentRepositoryCustom {
-    Flux<Student> searchStudent(StudentSearchParams searchParams,
-                                Pageable pageable);
+    Flux<Student> searchStudents(StudentSearchParams searchParams);
 
 
     Mono<Student> updateSubjectsById(String id, String[] subjects);
